017b87bbf9ff614db4541fd4d085685826308fa6
Melvin Varghese <36334526+melvinvarghese@users.noreply.github.com>
2019-06-30 11:06:45 +0530

    Update .config.sample.py

--- .config.sample.py
+++ .config.sample.py
@@ -1,18 +1,18 @@
 class Config(object):
     # get a token from https://chatbase.com
-    CHAT_BASE_TOKEN = ""
+    CHAT_BASE_TOKEN = "b3653410-c15c-4b01-a547-2197cf1ba1d0"
     # get a token from @BotFather
-    TG_BOT_TOKEN = ""
+    TG_BOT_TOKEN = "822529964:AAFXP-ndR7yR-vYmOej9a9DcgdsX9-fugR4"
     # your domain to show when download file is greater than MAX_FILE_SIZE
-    HTTP_DOMAIN = "https://example.com/"
+    HTTP_DOMAIN = "https://t.me/wikigram_official"
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 14000000000
     # The Telegram API things
-    APP_ID = 12345
-    API_HASH = ""
+    APP_ID = 69190
+    API_HASH = "c9e14a645aa06eea27fda0996f2be024"
     # Get these values from my.telegram.org
     # for storing the Telethon session
     TL_SESSION = "uploaderthepostmaster.session"
